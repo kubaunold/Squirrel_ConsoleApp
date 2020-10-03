3c3eb346462eac91cf2e036572eb912eb2afd6ab
Kuba Unold <jakub@unold.pl>
2020-10-04 00:58:03 +0200

    Updated World.fs with code. Going to sleep.

--- Squirrel_DomainLogic/Actors.fs
+++ Squirrel_DomainLogic/Actors.fs
@@ -1,2 +1,17 @@
-﻿module Actors
+﻿namespace Squirrel_DomainLogic
 
+open Squirrel_DomainLogic.WorldPos
+
+module Actors =
+
+  [<AbstractClass>]
+  type Actor(pos: WorldPos) =
+    member this.Pos = pos
+    abstract member Character: char
+
+  type Squirrel(pos: WorldPos, hasAcorn: bool) =
+    inherit Actor(pos)
+    member this.HasAcorn = hasAcorn
+    override this.Character = 'S'
+
+  let createSquirrel pos = new Squirrel(pos, false)
--- Squirrel_DomainLogic/World.fs
+++ Squirrel_DomainLogic/World.fs
@@ -1,2 +1,32 @@
-﻿module World
+﻿namespace Squirell_DomainLogic
 
+open System
+open Squirrel_DomainLogic.Actors
+open Squirrel_DomainLogic.WorldPos
+
+module World =
+    // parentheses are very important here
+    // thanks to them there is no memoization (re-evaluating the random is enabled)
+  let getRandomPos(maxX:int32, maxY:int32, random: Random): WorldPos =
+    let x = random.Next(maxX) + 1
+    let y = random.Next(maxY) + 1
+    newPos x y
+
+  let generate (maxX:int32, maxY:int32, random: Random): Actor seq =
+    let pos = getRandomPos(maxX, maxY, random)
+    seq {
+      yield createSquirrel pos
+    }
+
+  type World (maxX: int32, maxY: int32, random: Random) = 
+    let actors = generate(maxX, maxY, random)
+    member this.Actors = actors
+    member this.MaxX = maxX
+    member this.MaxY = maxY
+
+    member this.GetCharacterAtCell(x, y) =
+      let mutable char = '.'
+      for actor in this.Actors do
+        if actor.Pos.X = x && actor.Pos.Y = y then
+          char <- actor.Character
+      char

b14be00558c2c111a06c8cd49eca885af1b1dd84
Kuba Unold <jakub@unold.pl>
2020-10-08 20:20:39 +0200

    Successfully ran 2 xUnit tests

--- Squirrel_xUnitTests/Tests.fs
+++ Squirrel_xUnitTests/Tests.fs
@@ -1,8 +1,23 @@
-module Tests
+module Squirrel_xUnitTests.Tests
 
 open System
+open Squirrel_DomainLogic.WorldPos
 open Xunit
 
-[<Fact>]
-let ``My test`` () =
-    Assert.True(true)
+//[<Fact>]
+//let ``My test`` () =
+//    Assert.True(true)
+
+[<Theory>]
+[<InlineData(4, 2, 4, 1, true)>]
+[<InlineData(4, 2, 4, 0, false)>]
+let ``Point Adjacency tests`` (x1, y1, x2, y2, expectedAdjacent) =
+    // Arrange
+    let pos1 = newPos x1 y1
+    let pos2 = newPos x2 y2
+
+    // Act; adjacent - s¹siaduj¹cy
+    let isAdjacent = isAdjacentTo pos1 pos2
+
+    // Assert
+    Assert.Equal(expectedAdjacent, isAdjacent)
